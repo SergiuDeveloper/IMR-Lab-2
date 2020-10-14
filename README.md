44a7129aaf42d25840322e82daec0220d842d509
SergiuDev <sergiu.nistor@info.uaic.ro>
2020-10-14 23:14:59 +0300

    Base functionality implemented

---
+++ README.md
@@ -0,0 +1,20 @@
+# IMR-Lab-2
+
+# Tema realizata de:
+# Nistor Marian-Sergiu(3B6)
+# Pascariu Diana Mihaela(3B6)
+
+Screenshots cu aplicatia:\
+https://ibb.co/tKvWPpG \
+https://ibb.co/8Mn6NCq \
+https://ibb.co/GPCmXp6 \
+https://ibb.co/Mhkg0YF \
+
+Aplicatia implica o scena in cadrul careia se folosesc elemente caracteristice VR Toolkit.\
+
+In fata jucatorului se afla o masa. Pe masa se afla un obiect care va fi folosit spre a desena trail-ul.\
+
+Trail-ul este realizat cu ajutorul unui trail renderer, care, by default, are field-ul de "emitting" pe valoarea false.\
+Pentru a desena trail-ul, jucatorul trebuie sa ia obiectul in mana(grab), iar apoi trebuie sa il foloseasca(use).\
+In momentul in care obiectul respectiv devine "used", trail render-ul trece pe "emitting". Cand devine unused, "emitting" ia din nou valoarea false.\
+Key-ul pentru actiunea de "use" este overridden pe tasta "E", in cadrul simulatorului.\
---
+++ VR Hello World/Assets/LineDrawScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VRTK;
+
+public class LineDrawScript : MonoBehaviour
+{
+    private TrailRenderer trailRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailRenderer = GetComponent<TrailRenderer>();
+        trailRenderer.emitting = false;
+
+        VRTK_InteractableObject interactableObject = GetComponent<VRTK_InteractableObject>();
+        interactableObject.InteractableObjectUsed += new InteractableObjectEventHandler(ObjectUsedCallback);
+        interactableObject.InteractableObjectUnused += new InteractableObjectEventHandler(ObjectUnusedCallback);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void ObjectUsedCallback(object sender, InteractableObjectEventArgs e)
+    {
+        trailRenderer.emitting = true;
+    }
+
+    private void ObjectUnusedCallback(object sender, InteractableObjectEventArgs e)
+    {
+        trailRenderer.emitting = false;
+    }
+}
